63bd0cbe1c20220c68c1776ee5b3014e12712866
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2024-04-24 16:43:21 +0100

    Add image preview and links

---
+++ app/(routes)/[slug]/page.js
@@ -0,0 +1,28 @@
+export default function Page({ params }) {
+  const imageWithTagSrc = `https://firebasestorage.googleapis.com/v0/b/frcapparence.appspot.com/o/${params.slug}-b.jpg?alt=media&token=b3e78d49-967d-471c-bc70-bad4c54d28a2`
+  const imageWithoutTagSrc = `https://firebasestorage.googleapis.com/v0/b/frcapparence.appspot.com/o/${params.slug}-a.jpg?alt=media&token=b3e78d49-967d-471c-bc70-bad4c54d28a2`
+  return (
+    <main className='flex flex-col items-center'>
+      <img className='w-24 absolute right-4 top-4' src='/images/logo.png'></img>
+      <div className='mt-24 mb-8 p-4 px-12 bg-orange font-bold'>
+        TAP THUMBNAILS TO OPEN
+      </div>
+      <section className='w-1/2 flex flex-col gap-8'>
+        <a href={imageWithTagSrc} target='_blank' rel='noopener noreferrer'>
+          <img
+            src={imageWithTagSrc}
+            className='w-full max-h-52 object-cover object-bottom'
+            alt='Image with tag'
+          />
+        </a>
+        <a href={imageWithTagSrc} target='_blank' rel='noopener noreferrer'>
+          <img
+            src={imageWithoutTagSrc}
+            className='w-full max-h-52 object-cover object-bottom'
+            alt='Image without tag'
+          />
+        </a>
+      </section>
+    </main>
+  )
+}
--- app/(routes)/page.js
+++ app/(routes)/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className=''>
-      <h1 className=''>Hello World</h1>
+      <h1 className=''>Face Swap Viewer</h1>
     </main>
   )
 }
