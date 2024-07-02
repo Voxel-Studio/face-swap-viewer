46a47969d1787e2d8bc0acdd0c6582fda5298a3e
CallumGorton <115784663+CallumGorton@users.noreply.github.com>
2024-07-02 13:31:09 +0100

    a

--- app/(routes)/[slug]/page.js
+++ app/(routes)/[slug]/page.js
@@ -1,28 +1,37 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 export default function Page({ params }) {
   const imageWithTagSrc = `https://firebasestorage.googleapis.com/v0/b/frcapparence.appspot.com/o/${params.slug}-b.jpg?alt=media&token=b3e78d49-967d-471c-bc70-bad4c54d28a2`
   const imageWithoutTagSrc = `https://firebasestorage.googleapis.com/v0/b/frcapparence.appspot.com/o/${params.slug}-a.jpg?alt=media&token=b3e78d49-967d-471c-bc70-bad4c54d28a2`
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push(imageWithoutTagSrc)
+  }, [])
   return (
-    <main className='flex flex-col items-center'>
-      {/*<img className='w-24 absolute right-4 top-4' src='/images/logo.png'></img>
-      <div className='mt-24 mb-8 p-4 px-12 bg-orange font-bold'>
-        TAP THUMBNAILS TO OPEN
-      </div>*/}
-      <section /*className='w-1/2 flex flex-col gap-8'*/>
-        <a href={imageWithTagSrc} target='_blank' rel='noopener noreferrer'>
-          {/*<img
-            src={imageWithTagSrc}
-            className='w-full max-h-52 object-cover object-bottom'
-            alt='Image with tag'
-          />*/}
-        </a>
-        <a href={imageWithoutTagSrc} target='_blank' rel='noopener noreferrer'>
-          <img
-            src={imageWithoutTagSrc}
-            className='w-full max-h-52 object-cover object-bottom'
-            alt='Image without tag'
-          />
-        </a>
-      </section>
-    </main>
+    <></>
+    // <main className='flex flex-col items-center'>
+    //   {/*<img className='w-24 absolute right-4 top-4' src='/images/logo.png'></img>
+    //   <div className='mt-24 mb-8 p-4 px-12 bg-orange font-bold'>
+    //     TAP THUMBNAILS TO OPEN
+    //   </div>*/}
+    //   <section /*className='w-1/2 flex flex-col gap-8'*/>
+    //     <a href={imageWithTagSrc} target='_blank' rel='noopener noreferrer'>
+    //       {/*<img
+    //         src={imageWithTagSrc}
+    //         className='w-full max-h-52 object-cover object-bottom'
+    //         alt='Image with tag'
+    //       />*/}
+    //     </a>
+    //     <a href={imageWithoutTagSrc} target='_blank' rel='noopener noreferrer'>
+    //       <img
+    //         src={imageWithoutTagSrc}
+    //         className='w-full max-h-52 object-cover object-bottom'
+    //         alt='Image without tag'
+    //       />
+    //     </a>
+    //   </section>
+    // </main>
   )
 }
